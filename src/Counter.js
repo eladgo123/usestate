ae3196ca5db37306976c468ccaff2e63aba8a654
eladgo123 <eladgolan8@gmail.com>
2022-11-20 12:29:04 +0200

    Reset Button

--- src/Counter.js
+++ src/Counter.js
@@ -8,13 +8,21 @@ function Counter() {
                 return oldCount+1
             } 
         )
+    }  
         console.log(count)
+    function rese(){
+          setCount(
+              function(res0){
+                return 0
+              }
+          )
     }
     return (
       <div >
         <h1>Counter</h1>
         <p>Counter is at {count}</p>
         <button onClick={incr}>Click to add 1 to counter</button>
+        <button onClick={rese}>reset</button>
       </div>
     );
   }
